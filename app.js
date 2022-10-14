d6c45e082d020051c3f6f53c9cdb5c626eb597a2
Kedar Agate <kedar.agate@gmail.com>
2022-10-14 08:46:46 +0530

    building logic

--- app.js
+++ app.js
@@ -1,17 +1,74 @@
 function convertToRoman(num) {
   let roman = "";
-  if (num > 0 && num < 4) {
-    for (let i = 1; i <= num; i++) {
-      roman = "I".repeat(i);
+  if (num > 0 && num <= 30)
+    if (num < 4) {
+      for (let i = 1; i <= num; i++) {
+        roman = "I".repeat(i);
+      }
     }
-  }
-
-  return (num = roman);
 }
 
-const result = convertToRoman(2);
+function checkLengthOfNumber(num) {
+  let length = num.toString().length;
+  return Number(length);
+}
 
-console.log(result);
+function checkNumberLastDigit(num) {
+  let slicedNum = num.toString().slice(-1);
+  return Number(slicedNum);
+}
+
+function romanNumberOneToNine(num) {
+  let romanNumber;
+  if (num < 4 && num > 0) {
+    for (let i = 1; i <= num; i++) {
+      romanNumber = "I".repeat(i);
+    }
+    return romanNumber;
+  }
+  if (num === 4) {
+    return (romanNumber = "IV");
+  }
+  if (num === 5) {
+    return (romanNumber = "V");
+  }
+  if (num === 6) {
+    return (romanNumber = "VI");
+  }
+  if (num === 7) {
+    return (romanNumber = "VII");
+  }
+  if (num === 8) {
+    return (romanNumber = "VIII");
+  }
+  if (num === 9) {
+    return (romanNumber = "IX");
+  }
+  if (num <= 0) {
+    return (romanNumber = "X");
+  }
+}
+function romanBelow39(num) {
+  let firstNum;
+  let roman;
+  let lastRoman;
+  if (num <= 39 && num >= 11) {
+    firstNum = num.toString().slice(-2, 1);
+    console.log(firstNum);
+    lastRoman = checkNumberLastDigit(num);
+    roman = romanNumberOneToNine(firstNum) + romanNumberOneToNine(lastRoman);
+    console.log(
+      romanNumberOneToNine(firstNum),
+      romanNumberOneToNine(lastRoman)
+    );
+
+    return roman;
+  } else {
+    return romanNumberOneToNine(num);
+  }
+}
+
+console.log(romanBelow39(10));
 
 // Roman numerals	Arabic numerals
 // M	1000
@@ -27,3 +84,5 @@ console.log(result);
 // V	5
 // IV	4
 // I	1
+
+// numbers up to 38
